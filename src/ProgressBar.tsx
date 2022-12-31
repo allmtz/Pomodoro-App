58f516bf826f97eb7c572113543ac8c12c94b9e6
Allan Martinez <martinezallan123@gmail.com>
2022-12-30 19:10:42 -0800

    WIP

---
+++ src/ProgressBar.tsx
@@ -0,0 +1,12 @@
+export const ProgressBar = () => {
+    return(
+    <>
+    <div className="my-10 w-[200px] h-[200px] bg-blue-800 rounded-full flex items-center justify-center">
+        <div className="w-[10px] h-[100px] bg-hl origin-[50% 100%] rotate-45"> </div>
+    </div>
+    
+    
+    
+    </>
+    )
+}
