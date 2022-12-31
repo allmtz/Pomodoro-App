aa7a1d6ee73fb963b154ef6bf58879a9f499d2e5
Allan Martinez <martinezallan123@gmail.com>
2022-12-31 13:35:29 -0800

    can open/close settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import './App.css'
 import { ProgressBar } from './ProgressBar'
 
@@ -7,6 +8,19 @@ let focus = "pomodoro"
 const focusedStyling = "bg-hl p-4 rounded-full text-dark-bg"
 
 function App() {
+  const settingsRef = useRef<HTMLDivElement>(null)
+
+  function openSettings(){
+    if(settingsRef.current){
+      settingsRef.current.style.display = "block"
+    }
+  }
+
+  function closeSettings(){
+    if(settingsRef.current){
+      settingsRef.current.style.display = "none"
+    }
+  }
   return (
     <>
       <div className="container flex flex-col justify-center align-center gap-10">
@@ -26,31 +40,32 @@ function App() {
           <p className='text-2xl tracking-[6px]'>PAUSE</p>
         </main>
 
-        <footer className='mx-auto my-16'>
-          <img src="src/assets/icon-settings.svg" alt="" />
+        <footer className='mx-auto my-16 cursor-pointer'>
+          <img onClick={openSettings} src="src/assets/icon-settings.svg" alt="" />
         </footer>
       </div>
 
-      <div className="FULLPAGE  font-kumbh w-full p-4 bg-white border-8 border-orange-300">
+      <div ref={settingsRef} className="FULLPAGE hidden font-kumbh w-full h-full absolute top-0 right-0 p-4 bg-grayed-out">
        
-        <div className='border-b-2 flex justify-between pb-4'>
-          <p className='text-xl' >Settings</p>
-          <p className='text-slate-400 font-extralight'>X</p>
-        </div>
-        
-        <p className='text-center my-5 tracking-widest'>TIME (MINUTES)</p>
-        <form action="" className="flex flex-col gap-5">
-
-          <div className="flex justify-around">
-            <p className='text-gray-400'>pomodoro</p>
-            <select name="pomodoro-length" className='bg-off-white pr-16 pl-2 py-1 rounded-md border' >
-              <option value="25">25</option>
-              <option value="30">30</option>
-              <option value="35">35</option>
-              <option value="40">40</option>
-              <option value="45">45</option>
-            </select>
+       <div className="SETTINGS-CARD w-[350px] mx-auto p-4 bg-orange-300">
+          <div className='border-b-2 flex justify-between pb-4'>
+            <p className='text-xl' >Settings</p>
+            <p className='text-slate-400 font-extralight cursor-pointer' onClick={closeSettings}>X</p>
           </div>
+        
+          <p className='text-center my-5 tracking-widest'>TIME (MINUTES)</p>
+          <form action="" className="flex flex-col gap-5">
+
+            <div className="flex justify-around">
+              <p className='text-gray-400'>pomodoro</p>
+              <select name="pomodoro-length" className='bg-off-white pr-16 pl-2 py-1 rounded-md border' >
+                <option value="25">25</option>
+                <option value="30">30</option>
+                <option value="35">35</option>
+                <option value="40">40</option>
+                <option value="45">45</option>
+              </select>
+            </div>
 
           <div className="flex justify-around">
             <p className='text-gray-400'>short break</p>
@@ -103,6 +118,7 @@ function App() {
 
           <button type='submit' className='bg-hl w-min mt-5 -mb-10 mx-auto px-8 py-2 rounded-full text-white'>Apply</button>
         </form>
+       </div>
       </div>
     </>
   )
