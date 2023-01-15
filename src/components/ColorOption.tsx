b7327ed0607700f1b8b87b2705c72714a1e934d5
Allan <martinezallan123@gmail.com>
2023-01-14 17:55:22 -0800

    colors pt3

--- src/components/ColorOption.tsx
+++ src/components/ColorOption.tsx
@@ -5,9 +5,25 @@ interface IColorOptionProps{
     color:string
 }
 
+export interface IColorVariants{
+    hl:string,
+    teal:string,
+    violet:string,
+}
+
 export const ColorOption = ( {colorRef, selectedColor, setSelectedColor, color}:IColorOptionProps ) => {
+    // https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
+    const colorVariants:IColorVariants = {
+        "hl": "bg-hl w-10 h-10 rounded-full flex justify-center items-center cursor-pointer" ,
+        "teal": "bg-teal w-10 h-10 rounded-full flex justify-center items-center cursor-pointer",
+        "violet": "bg-violet w-10 h-10 rounded-full flex justify-center items-center cursor-pointer",
+    }
+
+    // https://stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b
+    // colorVariants[color as keyof IColorVariants] 
+
     return(
-        <div className={`bg-${color} w-10 h-10 rounded-full flex justify-center items-center cursor-pointer`}
+        <div className={colorVariants[color as keyof IColorVariants]}
                 onClick={() =>{setSelectedColor(color); colorRef.current=color}}
                 >
             <div className={`${selectedColor===color ? "scale-1" : "scale-0"} duration-200`}>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
 import { ISettings } from "../App"
+import { IColorVariants } from "./ColorOption"
 
 interface INavProps{
     mode:string,
@@ -7,43 +7,29 @@ interface INavProps{
     settings: ISettings,
 }
 
-export const Nav = ( { mode, manuallyChangeMode, settings }:INavProps ) => {
+const defaultStyling = "cursor-pointer p-4 text-center" 
 
-    
-    // const focusedStyling = `bg-${settings.color} p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700`
-    const defaultStyling = "cursor-pointer p-4 text-center" 
+export const Nav = ( { mode, manuallyChangeMode, settings }:INavProps ) => {
+    const colorOptions:IColorVariants = {
+        hl: "bg-hl p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700",
+        teal: "bg-teal p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700",
+        violet: "bg-violet p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700",
+    }
 
     return(
         <nav className='flex max-sm:text-xs'>
             <ul className='flex items-center justify-around px-4 py-2 gap-9 text-light-purple font-bold bg-dark-bg rounded-full m-auto xl:scale-110'>
-                <li className={mode === "pomoLength" ? `bg-${settings.color} p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700` : defaultStyling}
+                <li className={mode === "pomoLength" ? colorOptions[settings.color as keyof IColorVariants]: defaultStyling}
                 onClick={()=>manuallyChangeMode("pomoLength")}
                 >pomodoro
-
-                {settings.color}</li>
-
-                <li className={mode === "shortBreak" ? `bg-${settings.color} p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700`  : defaultStyling}
+                </li>
+                <li className={mode === "shortBreak" ? colorOptions[settings.color as keyof IColorVariants] : defaultStyling}
                 onClick={()=>manuallyChangeMode("shortBreak")}
                 >short break</li>
-                <li className={mode === "longBreak" ?  `bg-${settings.color} p-4 rounded-full text-dark-bg text-center cursor-pointer duration-700`  : defaultStyling}
+                <li className={mode === "longBreak" ?  colorOptions[settings.color as keyof IColorVariants] : defaultStyling}
                 onClick={()=>manuallyChangeMode("longBreak")}
                 >long break</li>
             </ul>
         </nav>
     )
 }
-
-
-{/* <nav className='flex max-sm:text-xs'>
-<ul className='flex items-center justify-around px-4 py-2 gap-9 text-light-purple font-bold bg-dark-bg rounded-full m-auto xl:scale-110'>
-  <li className={mode === "pomoLength" ? focusedStyling : "cursor-pointer p-4 text-center"}
-    onClick={()=>manuallyChangeMode("pomoLength","pomoLength")}
-  >pomodoro</li>
-  <li className={mode === "shortBreak" ? focusedStyling : "cursor-pointer p-4 text-center"}
-    onClick={()=>manuallyChangeMode("shortBreak","shortBreak")}
-  >short break</li>
-  <li className={mode === "longBreak" ? focusedStyling : "cursor-pointer p-4  text-center"}
-    onClick={()=>manuallyChangeMode("longBreak","longBreak")}
-  >long break</li>
-</ul>
-</nav> */}
