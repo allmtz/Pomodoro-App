cc44263debc72b223b98d4a2836ab7fdc3bcb4ae
Allan Martinez <martinezallan123@gmail.com>
2023-03-21 17:04:13 -0700

    styling add task modal

--- src/components/AddTaskModal.tsx
+++ src/components/AddTaskModal.tsx
@@ -8,25 +8,37 @@ export const AddTaskModal = ({
   addTask,
 }: any) => {
   return (
-    <form className="flex flex-col text-gray-500 border p-4">
-      <div
-        className="cursor-pointer ml-auto"
-        onClick={() => setShowTaskModal(false)}
-      >
-        X
-      </div>
-      <label htmlFor="task-title">What's next?</label>
-      <input id="task-title" ref={taskTitleRef} />
-      <label htmlFor="estimated-pomos">Estimated Pomodoros?</label>
-      <input
-        type="number"
-        min="0"
-        step="1"
-        defaultValue="1"
-        id="estimated-pomos"
-        ref={estPomosRef}
-      />
-      <button onClick={addTask}>Add Task</button>
-    </form>
+    <div className="MODAL-WRAPPER fixed w-full h-full top-0 left-0 flex justify-center items-center bg-grayed-out">
+      <form className="flex flex-col gap-2 p-4 bg-white rounded-2xl">
+        <div
+          className="cursor-pointer ml-auto"
+          onClick={() => setShowTaskModal(false)}
+        >
+          X
+        </div>
+        <label htmlFor="task-title">What's next?</label>
+        <input
+          className="bg-off-white border rounded-md outline-none p-1 "
+          id="task-title"
+          ref={taskTitleRef}
+        />
+        <label htmlFor="estimated-pomos">Estimated Pomodoros?</label>
+        <input
+          className="bg-off-white border rounded-md outline-none p-1 "
+          type="number"
+          min="0"
+          step="1"
+          defaultValue="1"
+          id="estimated-pomos"
+          ref={estPomosRef}
+        />
+        <button
+          className="mt-2 border bg-green-400 hover:bg-green-300 rounded-md"
+          onClick={addTask}
+        >
+          Add Task
+        </button>
+      </form>
+    </div>
   );
 };
